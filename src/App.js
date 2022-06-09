0f05d73f2ff24f15a2eb2c78596ee86c90498e59
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-06-10 00:43:51 +0200

    Changed baseUrl from localhost to herokuApi

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
-  const baseUrl = 'http://localhost:8000';
+  const baseUrl = 'https://todo-react-app-sproof.herokuapp.com/api';
 
   useEffect(() => {
     filterHandler();
